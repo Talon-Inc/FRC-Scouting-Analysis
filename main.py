f9fea5c80d42833ccb866994450c18de221b07fa
Jonathan Labrum <jonathan.d.labrum@gmail.com>
2024-03-20 09:25:43 -0500

    Add PySimpleGui starter code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-
+import PySimpleGUI as sq
 
 def average_amp_scoring(data, team, last_num_of_games, sep):
     results = 0
@@ -235,3 +235,23 @@ with open(file_path) as file:
         else:
             print("Please choose a valid choice.\n")
             continue
+
+# All the stuff inside your window.
+layout = [  [sg.Text("What's your name?")],
+            [sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('Hello Example', layout)
+
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+
+    # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+
+    print('Hello', values[0], '!')
+
+window.close()
