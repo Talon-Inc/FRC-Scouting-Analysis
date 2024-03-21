246d2b7e7da0c32880687b827f8ccd6b31e1b6d2
Jonathan Labrum <jonathan.d.labrum@gmail.com>
2024-03-21 02:30:31 -0500

    Added master dictionary, and changed some functions to use cleaner data

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import csv
-import PySimpleGUI as sq
+import PySimpleGUI as sg
 
 def average_amp_scoring(data, team, last_num_of_games, sep):
     results = 0
     count = 0
+
+    for item in data['auto scoring']:
+        if item["team"] == str(team):
+            count += 1
+            if sep != 2:
+                for i in range(len(item["auto scoring"])):
+                    if item["auto scoring"][i] == "a":
+                        if item["auto scoring"][i + 1] == "s":
+                            results += 1
+            if sep != 1:
+                for b in range(len(item["teleop scoring"])):
+                    if item["teleop scoring"][b] == "a":
+                        if item["teleop scoring"][b + 1] == "s":
+                            results += 1
+            if count == last_num_of_games:
+                return str(team) + " scored speaker " + str(results / count) + " times per match in the last " \
+                       + str(last_num_of_games) + " matches.\n"
+
     r_list = []
     for row in data:
         r_list.insert(0, row)
@@ -26,7 +44,6 @@ def average_amp_scoring(data, team, last_num_of_games, sep):
     if count == 0:
         return str(team) + " did not attend this event.\n"
     return str(team) + " scores amp " + str(results / count) + " times per match on average.\n"
-
 
 def average_speaker_scoring(data, team, last_num_of_games, sep):
     results = 0
@@ -53,7 +70,6 @@ def average_speaker_scoring(data, team, last_num_of_games, sep):
     if count == 0:
         return str(team) + " did not attend this event.\n"
     return str(team) + " scores speaker " + str(results / count) + " times per match on average.\n"
-
 
 def amp_percentage(data, team, last_num_of_games, sep):
     made = 0
@@ -92,7 +108,6 @@ def amp_percentage(data, team, last_num_of_games, sep):
     if count == 0:
         return str(team) + " did not attend this event.\n"
     return str(team) + " made " + str((made / total) * 100) + "% of their amp shots.\n"
-
 
 def speaker_percentage(data, team, last_num_of_games, sep):
     made = 0
@@ -133,65 +148,77 @@ def speaker_percentage(data, team, last_num_of_games, sep):
         return str(team) + " did not attend this event.\n"
     return str(team) + " made " + str((made / total) * 100) + "% of their speaker shots.\n"
 
-
 def average_auto(data, team, last_num_of_games):
     points = 0
     count = 0
-    r_list = []
-    for row in data:
-        r_list.insert(0, row)
-    for item in r_list:
-        if item["team"] == str(team):
-            count += 1
-            for i in range(len(item["auto scoring"])):
-                if item["auto scoring"][i] == "s" and item["auto scoring"][i + 1] == "s":
-                    points += 5
-                elif item["auto scoring"][i] == "a" and item["auto scoring"][i + 1] == "s":
-                    points += 2
-            if item["leave"] == "true":
-                points += 2
-            if count == last_num_of_games:
-                return str(team) + " scored " + str(points / count) + " points on average during auto\n"
+    for row in data[team]:
+        count += 1
+        for p in row["auto scoring"]:
+            if p == "ss":
+                points += 5 # score speaker
+            elif p == "as":
+                points += 2 # score amp
+        if row["leave"] == "true":
+            points += 2 # score leaving
+        if count == last_num_of_games:
+            return str(team) + " scored " + str(points / count) + " points on average during auto\n"
     if count == 0:
         return str(team) + " did not attend this event.\n"
     return str(team) + " scored " + str(points / count) + " points on average during auto\n"
 
-
 def average_endgame(data, team, last_num_of_games):
     points = 0
     count = 0
-    r_list = []
-    for row in data:
-        r_list.insert(0, row)
-    for item in r_list:
-        if item["team"] == str(team):
-            count += 1
-            for i in range(len(item["teleop scoring"])):
-                if item["teleop scoring"][i] == "t" and item["teleop scoring"][i + 1] == "s":
-                    points += 5
-            if item["stage level"] == '2':
-                if item["spotlight"] == "true":
-                    points += 4
-                else:
-                    points += 3
-            elif item["stage level"] == '1':
-                points += 1
-            elif item["stage level"] == '3':
+    for item in data[team]:
+        count += 1
+        for i in item['teleop scoring']:
+            if i == 'ts':
                 points += 5
-            if count == last_num_of_games:
-                return str(team) + " scored " + str(points / count) + " stage points on average\n"
+        if item['stage level'] == '2':
+            if item['spotlight'] == 'true':
+                points += 4
+            else:
+                points += 3
+        elif item['stage level'] == '1':
+            points += 1
+        elif item['stage level'] == '3':
+            points += 5
+        if count == last_num_of_games:
+            return str(team) + " scored " + str(points / count) + " stage points on average\n"
     if count == 0:
         return str(team) + " did not attend this event.\n"
     return str(team) + " scored " + str(points / count) + " stage points on average\n"
 
+def add_to_master_dict(master_dict, data):
+    team = data['team']
+    team_data = data.copy()
+    team_data.pop('')
+    team_data.pop('team')
+    team_data.pop('scouter')
+    team_data.pop('timestamp')
+    # convert auto and teleop scores to lists
+    auto_list = team_data['auto scoring'][1:-1].split(",")
+    team_data['auto scoring'] = [s.strip() for s in auto_list]
+    teleop_list = team_data['teleop scoring'][1:-1].split(",")
+    team_data['teleop scoring'] = [s.strip() for s in teleop_list]
 
-file_path = input("Please copy and paste the exact file path of the CSV file. ")
+    # if team doesn't exist, create team
+    if team not in master_dict:
+        master_dict[team] = []
+    # add data to team
+    master_dict[team].append(team_data)
+
+# file_path = input("Please copy and paste the exact file path of the CSV file: ")
+file_path = "C:\\Users\\tiger\\Downloads\\tpw-scouting-2024txpla.csv"
+data_dict = {}
 data_list = []
+exclude = []
 with open(file_path) as file:
     csv_reader = csv.DictReader(file)
-
-    for r in csv_reader:
-        data_list.append(r)
+    # print(csv_reader.restkey["4641"])
+    for data in csv_reader:
+        add_to_master_dict(data_dict, data)
+    print(data_dict['148'][-1])
 
     while True:
         choice = input("Enter a number, or type 'done' to end the program.\n"
@@ -200,7 +227,8 @@ with open(file_path) as file:
                        "3. Amp Percentage\n"
                        "4. Speaker Percentage\n"
                        "5. Average Auto Score\n"
-                       "6. Average Endgame Score\n")
+                       "6. Average Endgame Score\n"
+                       "Enter: ")
         if choice == "done":
             break
         elif choice in ["1", "2", "3", "4"]:
